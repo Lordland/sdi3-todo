10098e77cc3ab3f3940f3f153472d39d4a40ac2c
Palaskan <onecarlostlol@gmail.com>
2016-05-19 16:37:13 +0200

    subido cliente ejbasura

--- sdi3-40.EJB/ejbModule/uo/sdi/business/UserService.java
+++ sdi3-40.EJB/ejbModule/uo/sdi/business/UserService.java
@@ -1,5 +1,6 @@
 package uo.sdi.business;
 
+import java.util.List;
 import java.util.Map;
 
 import uo.sdi.model.User;
@@ -16,5 +17,7 @@ public interface UserService {
 	public void cerrarSesion(User usuario, Map<String ,Object> session);
 	
 	public void crearUsuario(User usuario, String comparaPass) throws Exception;
+
+	public List<User> getUsers();
 	
 }
